33d2be8aa468800ecb6684e7cfe111c8b58c9cd0
Destin Lee <dleeinc@gmail.com>
2021-03-07 14:34:35 -0600

    emotion

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
+/** @jsx jsx */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { SiReact } from 'react-icons/si'
+import { SiReact, SiWebpack, SiTypescript } from 'react-icons/si'
+
+import { css, jsx } from '@emotion/react'
 
 import './styles.scss'
+
+const iconsStyle = css`
+  display: flex;
+  font-size: 2rem;
+`
 
 function App() {
   return (
     <div>
-      <SiReact />
+      <div className="icons" css={iconsStyle}>
+        <SiWebpack />
+        <SiReact />
+        <SiTypescript />
+        <span>👩‍🎤</span>
+      </div>
       <h1>Welcome</h1>
     </div>
   )
